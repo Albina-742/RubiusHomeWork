89a5153244767b0ceb95091df9a5571a1b737be3
Albina S <Al.Shtennikova@gmail.com>
2024-11-04 18:15:00 +0700

    Задача по условному оператору

---
+++ Lesson2/Conditional_Operator/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите целое число от 0 до 30000");
+string Value = Console.ReadLine();
+int year = Convert.ToInt32(Value);
+
+if (year % 4 == 0 & year % 100 != 0 | year % 400 == 0)
+{
+    Console.WriteLine("YES");
+}
+else
+{
+    Console.WriteLine("NO");
+}
