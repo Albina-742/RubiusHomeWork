e78e8cadcfe448206d90144e8b65e94cb6216daf
Albina S <Al.Shtennikova@gmail.com>
2024-11-04 23:04:22 +0700

    Задача по циклам

---
+++ Lesson2/Cycles/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Пожалуйста, введите количество расходов: ");
+int numbers = int.Parse(Console.ReadLine());
+int sum = 0;
+
+for (int i = 1; i <= numbers; i++)
+{
+    Console.WriteLine($"Введите сумму расхода {i}: ");
+    int s = int.Parse(Console.ReadLine());
+    sum += s;
+ }
+
+Console.WriteLine($"Общая сумма расходов за месяц составляет:{sum}");
